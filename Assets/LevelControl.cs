ada443746c7a03f98d71ae54f8af771c513a4b52
erik_ritter <erik.ritter@airbnb.com>
2021-06-11 18:51:55 -0700

    Add LevelMaster and LevelControl

---
+++ Assets/LevelControl.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelControl : MonoBehaviour
+{
+    public int goalCount;
+    private bool[] goalStates;
+    // Start is called before the first frame update
+    void Start()
+    {
+        goalStates = new bool[goalCount];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateGoalState(int goalIndex, bool state)
+    {
+        goalStates[goalIndex] = state;
+
+        if (!Array.Exists(goalStates, state => state == false))
+        {
+            Debug.Log("You beat the level");
+            GlobalControl.Instance.GotoNextLevel();
+        }
+    }
+
+}
