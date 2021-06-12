6731fb53f35d1076a35162cb07bbe7d666a52414
Andrei Stanescu <reydan@gmail.com>
2021-06-11 22:06:49 -0700

    Add CameraController script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Camera cam;
+
+    public Transform[] markersToFit;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    private void LateUpdate()
+    {
+        if (markersToFit.Length == 0)
+        {
+            return;
+        }
+        Vector2 min = markersToFit[0].position;
+        Vector2 max = markersToFit[0].position;
+
+        foreach (Transform t in markersToFit)
+        {   
+            if (t.position.x < min.x)
+            {
+                min.x = t.position.x;
+            }
+            if (t.position.y < min.y)
+            {
+                min.y = t.position.y;
+            }
+            if (t.position.x > max.x)
+            {
+                max.x = t.position.x;
+            }
+            if (t.position.y > max.y)
+            {
+                max.y = t.position.y;
+            }
+        }
+
+        cam.orthographicSize = Mathf.Max((max.x - min.x) * Screen.height / Screen.width / 2, (max.y - min.y) / 2) + 0.5f;
+        cam.transform.position = (max + min) / 2;
+    }
+}
