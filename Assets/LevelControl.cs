9b0def38b171a0e5719ddec990f53d8e1421f303
erik_ritter <erik.ritter@airbnb.com>
2021-06-13 03:15:21 -0700

    Update level boarder prefab

--- Assets/LevelControl.cs
+++ Assets/LevelControl.cs
@@ -21,6 +21,7 @@ public class LevelControl : MonoBehaviour
         if (!Array.Exists(goals, goal => goal.isTriggered == false))
         {
             Debug.Log("You beat the level");
+            GameObject.Find("Canvas").GetComponent<Canvas>().enabled = false;
             GetComponent<AudioSource>().Play();
             isVictoryAnimation = true;
         }
